1890f0b2524a18a2d18e6567a00d79a37f942298
Brandon Yap <brandonyap96@gmail.com>
2020-04-06 15:17:21 -0400

    Added Random Strategy

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from strategies.PositionStrategy import PositionStrategy
 from strategies.AngleStrategy import AngleStrategy
 from strategies.TipVelocityStrategy import TipVelocityStrategy
 from strategies.TipVelocityAndPositionStrategy import TipVelocityAndPositionStrategy
+from strategies.RandomStrategy import RandomStrategy
 
 episodes = 100
+render=False
 
 def play_game(strategy, title="", episodes=100):
     print(title)
-    game = Game(episodes, strategy(), render=False)
+    game = Game(episodes, strategy(), render)
     game.play()
     game.close()
     game.plot(title)
@@ -19,3 +21,4 @@ if __name__ == '__main__':
     play_game(AngleStrategy, "Angle Strategy", episodes)
     play_game(TipVelocityStrategy, "Tip Velocity Strategy", episodes)
     play_game(TipVelocityAndPositionStrategy, "Tip Velocity and Position Strategy", episodes)
+    play_game(RandomStrategy, "Random Strategy", episodes)
