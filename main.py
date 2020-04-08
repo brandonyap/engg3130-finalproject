391e5973ae7c4944d7a64808f94fbbe84c75a8fc
Brandon Yap <brandonyap96@gmail.com>
2020-04-08 19:54:30 -0400

    Added Random and DQN Strategy and changed bootstrap for calculating scores (#3)
    * Added Q Learning strategy
    
    * Updated Strategy
    
    * Changed so that scores are counted and displayed.
    
    * Added Random Strategy
    
    * Minor Changes.
    
    * Fixed random strategy
    
    * updated to plot two graphs
    
    Co-authored-by: Dylan MacKenzie <dmackenz@uoguelph.ca>

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from strategies.PositionStrategy import PositionStrategy
 from strategies.AngleStrategy import AngleStrategy
 from strategies.TipVelocityStrategy import TipVelocityStrategy
 from strategies.TipVelocityAndPositionStrategy import TipVelocityAndPositionStrategy
+from strategies.RandomStrategy import RandomStrategy
 
-def play_game(strategy, title="", steps=1000):
-    game = Game(steps, strategy())
+episodes = 100
+render=False
+
+def play_game(strategy, title="", episodes=100):
+    print(title)
+    game = Game(episodes, strategy(), render)
     game.play()
     game.close()
     game.plot(title)
 
 if __name__ == '__main__':
-    play_game(PositionStrategy, "Position Strategy", 100)
-    play_game(AngleStrategy, "Angle Strategy", 100)
-    play_game(TipVelocityStrategy, "Tip Velocity Strategy", 100)
-    play_game(TipVelocityAndPositionStrategy, "Tip Velocity and Position Strategy", 300)
-
+    play_game(PositionStrategy, "Position Strategy", episodes)
+    play_game(AngleStrategy, "Angle Strategy", episodes)
+    play_game(TipVelocityStrategy, "Tip Velocity Strategy", episodes)
+    play_game(TipVelocityAndPositionStrategy, "Tip Velocity and Position Strategy", episodes)
+    play_game(RandomStrategy, "Random Strategy", episodes)
--- utils/Game.py
+++ utils/Game.py
@@ -1,39 +1,60 @@
+from utils.ScoreLogger import ScoreLogger
 from utils.Logger import Logger
 import gym
 
 class Game:
-    def __init__(self, steps, strategy):
-        self.steps = steps
+    def __init__(self, episodes, strategy, render=False):
+        self.episodes = episodes
         self.strategy = strategy
         self.logger = Logger()
+        self.scorelogger = ScoreLogger()
+        self.render = render
 
-        self.env = gym.make('CartPole-v0')
-        self.env.reset()
+        self.env = gym.make('CartPole-v1')
 
-    def log(self, observation):
+    def log_score(self, score):
+        self.scorelogger.log(score)
+
+    def log_observation(self, observation):
         self.logger.log(
-            self.strategy.get_pole_position(observation),
-            self.strategy.get_pole_velocity(observation),
-            self.strategy.get_pole_angle(observation),
-            self.strategy.get_pole_tip_velocity(observation)
-        )
+             self.strategy.get_pole_position(observation),
+             self.strategy.get_pole_velocity(observation),
+             self.strategy.get_pole_angle(observation),
+             self.strategy.get_pole_tip_velocity(observation)
+         )
 
     def print_logs(self):
-        print(self.logger.get_positions())
-        print(self.logger.get_velocities())
-        print(self.logger.get_angles())
-        print(self.logger.get_tip_velocities())
+         print(self.logger.get_positions())
+         print(self.logger.get_velocities())
+         print(self.logger.get_angles())
+         print(self.logger.get_tip_velocities())
+         print(self.scorelogger.get_scores())
 
     def plot(self, title=""):
+        self.scorelogger.plot(title)
         self.logger.plot(title)
 
     def play(self):
-        observation, reward, done, info = self.env.step(0)
-        for _ in range(self.steps):
-            self.env.render()
-            action = self.strategy.calculate(observation)
-            observation, reward, done, info = self.env.step(action)
-            self.log(observation)
+        for episode in range(self.episodes):
+            self.env.reset()
+            observation, reward, done, info = self.env.step(0)
+
+            step = 0
+
+            while True:
+                step += 1
+                if self.render:
+                    self.env.render()
+                action = self.strategy.calculate(observation)
+                observation, reward, done, info = self.env.step(action)
+
+                if episode == self.episodes - 1:
+                    self.log_observation(observation)
+
+                if done:
+                    self.log_score(step)
+                    print("Run: " + str(episode+1) + ", score: " + str(step))
+                    break
 
     def close(self):
         self.env.close()
