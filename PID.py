d273a4555af66f072039c1db1d2fb03e48eee69c
gwynedd <Gwyneth@Gwyns-MacBook-Air.local>
2020-04-15 15:57:29 -0400

    starting PID

---
+++ PID.py
@@ -0,0 +1,10 @@
+import gym
+import numpy as np 
+from utils.BaseStrategy import BaseStrategy
+#self.make_move_left_action(), self.make_move_right_action()
+
+#input is angle, output is force required to correct angle
+def PID_Controller(angle,dist):
+    #
+    return
+
